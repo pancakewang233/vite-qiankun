cdd96e95b7c78a3b2bc066f5f451421301fbae4d
pancake~ <834430775@qq.com>
2022-11-22 00:07:53 +0800

    micro container init

---
+++ packages/container/vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import unocss from 'unocss/vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueJsx(),
+    unocss(),
+  ]
+})
