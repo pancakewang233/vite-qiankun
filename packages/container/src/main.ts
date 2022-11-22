239c4da4c9df9b9841822800b8496b283c6af047
pancake~ <834430775@qq.com>
2022-11-23 00:30:01 +0800

    container unocss

--- packages/container/src/main.ts
+++ packages/container/src/main.ts
@@ -1,13 +1,19 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
 
 // router
 import router from './router/index'
 
-//unocss
+/** 重置样式 这里引入自定义的重置样式也可 */
+import '@unocss/reset/tailwind.css'
+/** 
+ *  项目内的样式，
+ *  注意：最好放在重置样式后，uno.css前
+ */
+import './style.css'
+/** 引入uno.css，不引入不生效 */
 import 'uno.css'
 
 createApp(App)
     .use(router)
-    .mount('#app')
+    .mount('#container')
--- packages/container/src/views/dashboard/index.vue
+++ packages/container/src/views/dashboard/index.vue
@@ -1,9 +1,16 @@
 <script setup lang="ts">
+import NavBar from 'src/components/Navbar.vue'
+import SideBar from 'src/components/SideBar.vue'
+import Main from 'src/components/Main.vue'
 </script>
 
 <template>
-  <div>
-    123
+  <div h-screen flex flex-col>
+    <NavBar h-20 border border-red-500 />
+    <div flex-grow flex >
+      <SideBar w-100 border border-red-500 />
+      <Main  flex-grow />
+    </div>
   </div>
 </template>
 
